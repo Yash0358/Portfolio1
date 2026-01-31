af0913e33f57f208e0860caea8a7b7de4f867503
Yash0358 <lanjewark22@gmail.com>
2026-01-31 23:15:44 +0530

    changed imaged location to public

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,10 @@
+import React from 'react';
+
 export const Hero = () => {
   return <section className=" relative min-h-screen overflow-hidden flex items-center">
     {/* bg */}
     <div>
-      <img src="/hero-bg.jpg" alt="" />
+      <img src="/hero-bg.jpg" alt="image" />
     </div>
   </section>
 }
