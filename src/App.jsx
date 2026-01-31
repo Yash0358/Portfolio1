6d4389d5bb30fa157624d54332756177c4767d09
Yash0358 <lanjewark22@gmail.com>
2026-01-31 22:53:22 +0530

    creating navbar and making it responsive for mobile

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Navbar} from './layout/Navbar'
+import {Hero} from './sections/Hero'
+import {About} from './sections/About'
+import {Contacts} from './sections/Contacts'
+import {Experience} from './sections/Experience'
+import {Testimonials} from './sections/Testimonials'
+import {Projects} from './sections/Projects'
+
+const App = () => {
+  return (
+    <div className=' min-h-screen overflow-x-hidden'>
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Contacts />
+        <Experience />
+        <Testimonials />
+        <Projects />
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,53 @@
+
+import { Menu ,X } from 'lucide-react'
+import { Button } from '../components/Button'
+import { useState } from 'react'
+
+const navlinks = [
+ { href: '#about',label: 'About'},
+ { href: '#experience',label: 'Experience'},
+ { href: '#testimonials',label: 'Testimonials'},
+ { href: '#projects',label: 'Projects'},
+]
+
+export const Navbar = () => {
+  const [ismobilemenuopen, setIsmobilemenuopen] =  useState(false);
+
+  return <header className="fixed top-0 left-0 right-0 py-5 bg-transparent">
+    <nav className="container mx-auto flex justify-between px-6">
+      <a href="#" className="font-bold text-xl tracking-tight hover:text-primary" >
+        Y076 <span>.</span>
+      </a>
+      {/* //Desktop Navbar// */}
+      <div className="hidden md:flex items-center gap-1">
+        <div className="glass rounded-full px-2 py-1 flex items-center gap-2">
+          {navlinks.map((link ,index) =>(
+            <a href={link.href} key={index} className="px-4 py-2 rounded-full text-sm text-muted-foreground hover:text-foreground hover:bg-surface"> {link.label} </a>
+          ))}
+        </div>
+      </div>
+
+      {/* CAT BUTTON */}
+      <div className='hidden md:block'>
+        <Button size="sm" >Contact Me</Button>
+        </div>
+          {/* mobile menu button */}
+        <button className='md:hidden p-2 text-foreground cursor-pointer' 
+         onClick={() => setIsmobilemenuopen((prev)=> !prev ) }
+         >
+          {ismobilemenuopen ? <X size={24}/> : <Menu size={24}/> }
+        </button>
+   </nav>
+
+        {/* mobile menu */}
+        {ismobilemenuopen && (<div className='md:hidden glass-strong animate-fade-in'>
+             <div className='container flex flex-col gap-2 py-6 px-6 mx-auto'>
+                {navlinks.map((link ,index) =>(
+            <a href={link.href} key={index} className="text-lg text-muted-foreground py-2"> {link.label} </a>
+          ))}
+        <Button size="sm" >Contact Me</Button>
+            </div>
+          </div>
+        )}
+  </header>
+}
